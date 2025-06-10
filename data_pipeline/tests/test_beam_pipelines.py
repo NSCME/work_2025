fc0197f50b0dd3d211879455aabcad49a19bbf07
NSCME <zachariazac01@gmail.com>
2025-06-10 11:00:11 -0500

    Update test_beam_pipelines.py
    Uses pytest and Beam’s TestPipeline

--- data_pipeline/tests/test_beam_pipelines.py
+++ data_pipeline/tests/test_beam_pipelines.py
@@ -1,1 +1,18 @@
+import pytest
+import apache_beam as beam
+from apache_beam.testing.test_pipeline import TestPipeline
+from apache_beam.testing.util import assert_that, equal_to
 
+def test_high_risk_filter():
+    test_data = [
+        {"credit_score": 500, "application_id": "app1"},
+        {"credit_score": 700, "application_id": "app2"}
+    ]
+
+    with TestPipeline() as p:
+        output = (
+            p
+            | beam.Create(test_data)
+            | beam.Filter(lambda app: app["credit_score"] < 600)
+        )
+        assert_that(output, equal_to([test_data[0]]))  # Only first app is high-risk
