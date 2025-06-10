ab74dce44383263eb530ce067942a164c1d88869
NSCME <zachariazac01@gmail.com>
2025-06-10 11:00:44 -0500

    Update test_data_validation.py
    Test Great Expectations

--- data_pipeline/tests/test_data_validation.py
+++ data_pipeline/tests/test_data_validation.py
@@ -1,1 +1,10 @@
+from expectations.loan_app_expectations import build_suite
 
+def test_expectations():
+    suite = build_suite()
+    assert "expect_column_to_exist" in [e.expectation_type for e in suite.expectations]
+
+def test_validation_failure():
+    bad_data = {"credit_score": 200}  # Below min
+    with pytest.raises(ValueError):
+        validate_data(bad_data)
