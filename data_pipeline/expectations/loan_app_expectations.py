370fd2ae85f8901afef1673646ac21c313ccf65a
NSCME <zachariazac01@gmail.com>
2025-06-10 10:57:02 -0500

    Validate schema, ranges, and business rules

--- data_pipeline/expectations/loan_app_expectations.py
+++ data_pipeline/expectations/loan_app_expectations.py
@@ -1,1 +1,23 @@
+from great_expectations.core import ExpectationSuite, ExpectationConfiguration
 
+def build_suite() -> ExpectationSuite:
+    suite = ExpectationSuite(name="loan_app_suite")
+
+    # Schema validation
+    suite.add_expectation(ExpectationConfiguration(
+        expectation_type="expect_column_to_exist",
+        kwargs={"column": "credit_score"}
+    ))
+
+    # Value bounds
+    suite.add_expectation(ExpectationConfiguration(
+        expectation_type="expect_column_values_to_be_between",
+        kwargs={"column": "credit_score", "min_value": 300, "max_value": 850}
+    ))
+
+    # Non-null check
+    suite.add_expectation(ExpectationConfiguration(
+        expectation_type="expect_column_values_to_not_be_null",
+        kwargs={"column": "application_id"}
+    ))
+    return suite
